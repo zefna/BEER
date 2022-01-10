5a06bda3ec46cb4677d61effb0c901c40cedd895
zefna <zefna@yandex.com>
2022-01-10 21:43:07 +0300

    new features for more wide beer searching

--- modules/matcher.py
+++ modules/matcher.py
@@ -7,7 +7,6 @@ from modules.db_client import _BaseClient, ClientFactory
 
 sns.set()
 
-
 class BeerMatcher:
     _db: _BaseClient
 
@@ -28,7 +27,39 @@ class BeerMatcher:
         beer_props = dict()
 
         print('Введите крепость пива')
-        beer_props['Алкоголь:'] = float(int(input()) / 100)
+        _in = input()
+        if len(_in) == 0:
+            pass
+        else:
+            beer_props['Алкоголь:'] = float(int(_in) / 100)
+
+        print('Введите плотность пива')
+        _in = input()
+        if len(_in) == 0:
+            pass
+        else:
+            beer_props['Плотность:'] = float(int(_in) / 100)
+
+        print('Введите цвет пива (светлое, тёмное, полутёмное)')
+        _in = input()
+        if len(_in) == 0:
+            pass
+        else:
+            beer_props['Цвет:'] = _in
+
+        print('Выберите фильтрацию (фильтрованное, нефильтрованное)')
+        _in = input()
+        if len(_in) == 0:
+            pass
+        else:
+            beer_props['Фильтрация:'] = _in
+
+        print('Введите страну изготовления')
+        _in = input()
+        if len(_in) == 0:
+            pass
+        else:
+            beer_props['Страна:'] = _in
 
         return beer_props
 
